a1e9896cc9d6a871c8df3dbc069523af1349cf03
monkeysees <monkeysees@once>
2024-12-22 16:23:47 +0400

    Handle `exit` command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,10 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
+            if (input.equals("exit 0")) {
+                scanner.close();
+                System.exit(0);
+            }
             System.out.println(String.format("%s: command not found", input));
         }
     }
