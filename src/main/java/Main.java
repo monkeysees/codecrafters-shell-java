458ec7b4b6bb77e6b711aae36d47044afdfa2d6d
monkeysees <monkeysees@once>
2024-12-22 16:38:12 +0400

    Refactor to parse input into command and args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
-            String input = scanner.nextLine();
-            if (input.equals("exit 0")) {
+            String[] input = scanner.nextLine().split(" ", 2);
+            String command = input[0];
+            String commandArg = (input.length > 1) ? input[1] : "";
+            if (command.equals("exit") && commandArg.equals("0")) {
                 scanner.close();
                 System.exit(0);
             }
-            System.out.println(String.format("%s: command not found", input));
+            System.out.println(String.format("%s: command not found", command));
         }
     }
 }
