74e8b0dbe3a80d0112151dbf5d6e7468b6ca35a5
monkeysees <monkeysees@once>
2024-12-22 18:44:34 +0400

    Implement `type` command for builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,14 +10,33 @@ public class Main {
             String[] input = scanner.nextLine().split(" ", 2);
             String command = input[0];
             String commandArg = (input.length > 1) ? input[1] : "";
-            if (command.equals("exit") && commandArg.equals("0")) {
-                scanner.close();
-                System.exit(0);
-            }
-            if (command.equals("echo")) {
-                System.out.println(commandArg);
-            } else {
-                System.out.println(String.format("%s: command not found", command));
+            switch (command) {
+                case "exit":
+                    if (commandArg.equals("0")) {
+                        scanner.close();
+                        System.exit(0);
+
+                    } else {
+                        break;
+                    }
+                case "echo":
+                    System.out.println(commandArg);
+                    break;
+                case "type":
+                    List<String> knownBuiltinCommands = new ArrayList<>();
+                    knownBuiltinCommands.add("exit");
+                    knownBuiltinCommands.add("echo");
+                    knownBuiltinCommands.add("type");
+
+                    if (knownBuiltinCommands.contains(commandArg)) {
+                        System.out.println(String.format("%s is a shell builtin", commandArg));
+                    } else {
+                        System.out.println(String.format("%s: not found", commandArg));
+                    }
+
+                    break;
+                default:
+                    System.out.println(String.format("%s: command not found", command));
             }
         }
     }
