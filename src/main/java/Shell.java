17a9dd74901e768e29aeebecb0376551919ba193
monkeysees <126164844+monkeysees@users.noreply.github.com>
2025-03-28 00:22:27 +0400

    Move method for reading input from Shell to Input

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -1,23 +1,12 @@
-// Setting the console raw mode for character-buffered input
-// instead of a line-buffered one is inspired by the following resource:
-// https://darkcoding.net/software/non-blocking-console-io-is-not-possible/
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Shell {
     File cwd;
     String homeDir;
-    static final Pattern COMMAND_BEGINNING_PATTERN = Pattern.compile("^\\s*(\\S+)$");
-    static final Character BELL_CHARACTER = '\u0007';
 
     Shell() {
         cwd = new File(System.getProperty("user.dir"));
@@ -60,7 +49,7 @@ public class Shell {
 
             while (true) {
                 System.out.print("$ ");
-                String input = readInput();
+                String input = Input.readInput();
                 System.out.println();
                 processInput(input);
             }
@@ -80,124 +69,6 @@ public class Shell {
         }
 
         System.exit(isError ? 1 : 0);
-    }
-
-    String readInput() throws IOException, UserInitiatedException {
-        StringBuilder input = new StringBuilder();
-        ByteArrayOutputStream bout;
-        int c;
-        Boolean isEscaped = false;
-        Integer consecutiveTabsCount = 0;
-
-        while (true) {
-            bout = new ByteArrayOutputStream();
-            c = System.in.read();
-
-            // abort
-            if (c == -1 || c == 3 || c == 4) {
-                throw new AbortException();
-            }
-
-            // new line
-            if ((c == 13 || c == 10) && !isEscaped) {
-                return input.toString();
-            }
-
-            // delete
-            if (c == 127) {
-                if (input.length() > 0) {
-                    input.deleteCharAt(input.length() - 1);
-                    System.out.print("\b \b");
-                    System.out.flush();
-                }
-                continue;
-            }
-
-            // tab autocomplete
-            if (c == 9 && input.length() > 0) {
-                consecutiveTabsCount++;
-                Matcher commandBeginningMatcher = COMMAND_BEGINNING_PATTERN.matcher(input);
-                String commandBeginning = commandBeginningMatcher.find() ? commandBeginningMatcher.group(1) : null;
-                if (commandBeginning != null) {
-                    List<String> autocompleteOptions = Stream
-                            .concat(BuiltinCommand.autocomplete(commandBeginning).stream(),
-                                    Executable.autocomplete(commandBeginning).stream())
-                            .distinct()
-                            .sorted()
-                            .toList();
-                    switch (autocompleteOptions.size()) {
-                        case 0 -> System.out.print(BELL_CHARACTER);
-                        case 1 -> {
-                            String autocompletedInput = autocompleteOptions.getFirst();
-                            String newInputPortion = autocompletedInput
-                                    .substring(commandBeginning.length())
-                                    .concat(" ");
-                            input.append(newInputPortion);
-                            System.out.print(newInputPortion);
-                        }
-                        default -> {
-                            String autocompletedInput = findLongestCommonPrefix(autocompleteOptions,
-                                    commandBeginning);
-                            if (autocompletedInput.length() > commandBeginning.length()) {
-                                String newInputPortion = autocompletedInput.substring(commandBeginning.length());
-                                input.append(newInputPortion);
-                                System.out.print(newInputPortion);
-                            } else {
-                                if (consecutiveTabsCount == 1) {
-                                    System.out.print(BELL_CHARACTER);
-                                } else {
-                                    System.out.println();
-                                    System.out.println(String.join(" " + " ", autocompleteOptions));
-                                    System.out.print("$ " + input);
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    System.out.print(BELL_CHARACTER);
-                }
-
-                continue;
-            }
-
-            bout.write(c);
-            String binaryRepresentation = Integer.toBinaryString(c);
-            if (binaryRepresentation.length() == 8) {
-                // two byte character
-                if (binaryRepresentation.startsWith("110")) {
-                    bout.write(System.in.read());
-                }
-                // three byte character
-                else if (binaryRepresentation.startsWith("1110")) {
-                    bout.write(System.in.read());
-                    bout.write(System.in.read());
-                }
-                // four byte character
-                else if (binaryRepresentation.startsWith("11110")) {
-                    bout.write(System.in.read());
-                    bout.write(System.in.read());
-                    bout.write(System.in.read());
-                }
-            }
-
-            if (c == 13 || c == 10) {
-                input.append('\n');
-                System.out.print("\r\n");
-            } else {
-                input.append(bout);
-                System.out.print(bout);
-            }
-
-            if (c == 92) {
-                isEscaped = !isEscaped;
-            } else if (isEscaped) {
-                isEscaped = false;
-            }
-
-            if (c != 9 && consecutiveTabsCount > 0) {
-                consecutiveTabsCount = 0;
-            }
-        }
     }
 
     void processInput(String input) throws UserInitiatedException {
@@ -259,25 +130,5 @@ public class Shell {
         if (!dir.isDirectory()) {
             throw new IllegalArgumentException("Not a directory");
         }
-    }
-
-    static private String findLongestCommonPrefix(List<String> coll, String basePrefix) {
-        String shortestEntry = coll.stream()
-                .sorted((s1, s2) -> Integer.compare(s1.length(), s2.length()))
-                .findFirst()
-                .orElse(null);
-
-        if (shortestEntry == null || shortestEntry.equals(basePrefix)) {
-            return basePrefix;
-        }
-
-        for (int prefixLen = shortestEntry.length(); prefixLen > basePrefix.length(); prefixLen--) {
-            String testPrefix = shortestEntry.substring(0, prefixLen);
-            if (coll.stream().allMatch(entry -> entry.startsWith(testPrefix))) {
-                return testPrefix;
-            }
-        }
-
-        return basePrefix;
     }
 }
